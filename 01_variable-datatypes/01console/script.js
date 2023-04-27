7c35e020c5b9ca1b4ff5cb41fda03b2a21c3cc67
unknown <ulricksanchez@gmail.com>
2023-04-27 15:11:15 +0800

    Initial Commit

---
+++ 01_variable-datatypes/01console/script.js
@@ -0,0 +1,28 @@
+/* alert("Hello from JS file") */
+
+/* Log a number */
+console.log(100);
+
+/* log string */
+console.log("Hellow World");
+
+/* log multiple variables */
+console.log(20, "Hello", true);
+
+/* Log a variable */
+const x = 100;
+console.log(x);
+
+/* console error and warning */
+console.error("Alert");
+console.warn("Warning");
+
+/* log object as a table */
+console.table({ name: "Ulrick", email: "ulricksanchez@gmail.com" });
+
+/* Group Console commands */
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warm("Warning");
+console.groupEnd();
