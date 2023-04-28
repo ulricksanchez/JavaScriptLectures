884e83b597ea558a253d00926819d279be11b4a6
unknown <ulricksanchez@gmail.com>
2023-04-28 14:51:30 +0800

    Initial Commit

---
+++ 01-variable-datatypes/13challenge2-html/script.js
@@ -0,0 +1,27 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+//For the SUM
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+//For the DIFFERENCE
+const diff = x - y;
+const differenceOutput = `${x} - ${y} = ${diff}`;
+console.log(differenceOutput);
+
+//For the PRODUCRT
+const prod = x * y;
+const productOutput = `${x} * ${y} = ${prod}`;
+console.log(productOutput);
+
+//For the QOUTIENT
+const qout = x / y;
+const qoutientOutput = `${x} / ${y} = ${qout}`;
+console.log(qoutientOutput);
+
+//For the REMAINDER
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);
